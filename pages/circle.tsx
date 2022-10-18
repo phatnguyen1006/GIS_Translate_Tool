d493afa9896b6c8fd719d8f6289cda3da4a7e654
P <phatnguyen876@gmail.com>
2022-10-18 23:55:29 +0700

    base

---
+++ pages/circle.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { useState } from 'react'
+import styles from '../styles/Home.module.css'
+
+const Circle: NextPage = () => {
+    const [input, setInput] = useState();
+
+    const drawCircle = () => {
+      return "This is circle line";
+    }
+
+  return (
+    <div className={styles.container}>
+      <div>This is input</div>
+      <div>{drawCircle()}</div>
+    </div>
+  )
+}
+
+export default Circle
