340d2a0f9811df80f65ba7e41e55a78541fdd917
PhatFPT <phatnt59@fpt.com.vn>
2022-10-24 16:11:27 +0700

    Add version

--- components/Documentation/index.tsx
+++ components/Documentation/index.tsx
@@ -6,6 +6,8 @@ export default function Documentation() {
     <div className={styles.documentContainer}>
       <h1>GIS_Translate Tool</h1>
       <hr />
+      <p>Version: 1.0.3</p>
+      <br />
       <p>Tools được phát triển trong 2 buổi trưa nên UI không được vừa mắt, mong quý bạn thông cảm :( xin cảm ơn</p>
     </div>
   );
