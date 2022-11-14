a16868acb8742832a8c8def81942b715a4066e6a
PhatFPT <phatnt59@fpt.com.vn>
2022-11-14 17:30:38 +0700

    Add insert

--- pages/insert.tsx
+++ pages/insert.tsx
@@ -9,18 +9,38 @@ const { TextArea } = Input;
 function Insert() {
 
     const [json, setJSON] = useState<string>("");
+    const [rerender, setRerender] = useState<string>("");
+    const [checkingData, setCheckingData] = useState();
 
     function submitBlockToServer() {
         let jsonStr = JSON.parse(json);
-        axios.post("http://localhost:8000/building", { json: jsonStr });
+        let rerenderObj = JSON.parse(rerender);
+        console.log(rerenderObj);
+        
+        
+        axios.post("http://localhost:8000/building", { json: jsonStr, rerender: rerenderObj }).then((res) => {
+            console.log(res);
+            setCheckingData(res.data.msg);
+        });
     }
 
     return (
         <div className={styles.container}>
-            <TextArea style={{ width: "80%" }} rows={40} placeholder="Nhap JSON" onChange={(e) => setJSON(e.target.value)} />
-            <Button type="primary" onClick={submitBlockToServer}>Primary</Button>
+            <div className={styles.headerCheckingContainer}>
+                <div className={styles.checkingTagContainer}>
+                    <p>Data của bạn</p>
+                    <p>{checkingData}</p>
+                    <Button type="primary" onClick={submitBlockToServer} style={{ marginRight: 20 }}>Correct</Button>
+                    <Button type="primary" onClick={submitBlockToServer}>Incorrect</Button>
+                </div>
+                <Button type="primary" onClick={submitBlockToServer}>Primary</Button>
+            </div>
+            <div className={styles.headerCheckingContainer}>
+                <TextArea style={{ width: "45%" }} rows={40} placeholder="Nhap JSON" onChange={(e) => setJSON(e.target.value)} />
+                <TextArea style={{ width: "45%" }} rows={40} placeholder="Nhap JSON rerender" onChange={(e) => setRerender(e.target.value)} />
+            </div>
         </div>
-    )
+    );
 }
 
 export default withAuth(Insert);
