101cfb18df08a518aad279228912caefdfd694ac
PhatFPT <phatnt59@fpt.com.vn>
2022-11-16 15:04:44 +0700

    tool _v1

--- pages/insert.tsx
+++ pages/insert.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import styles from '../styles/Insert.module.css';
-import { Button, Input, Modal } from 'antd';
+import { Button, Input, Modal, notification } from 'antd';
 import axios from 'axios';
 import withAuth from 'libs/privateRoute';
 
 const { TextArea } = Input;
 
+type NotificationType = 'success' | 'info' | 'warning' | 'error';
+
 interface ICheckingData {
     json: {
         id: string;
-        face: Array<any>;
+        nodes: Array<any>;
+        block_name: string;
+        block_height: number;
+        color: string;
     }
     log?: {
         _id: string;
@@ -24,7 +29,8 @@ function Insert() {
 
     const [json, setJSON] = useState<string>("");
     const [rerender, setRerender] = useState<string>("");
-    const [checkingData, setCheckingData] = useState<ICheckingData>({ json: { id: "", face: [] } });
+    const [checkingData, setCheckingData] = useState<ICheckingData>({ json: { id: "", nodes: [], block_name: "", block_height: 0, color: "" } });
+    const [responsePayload, setResponsePayload] = useState<string>();
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const showModal = () => {
@@ -33,10 +39,18 @@ function Insert() {
 
     const handleOk = () => {
         submitBlockToServer();
+        setIsModalOpen(false);
     };
 
     const handleCancel = () => {
         setIsModalOpen(false);
+    };
+
+    const openNotificationWithIcon = (type: NotificationType) => {
+        notification[type]({
+            message: 'Đã gửi data đi',
+            description: "Kết quả sẽ được trả về tại phần Data của bạn",
+        });
     };
 
     async function submitBlockToServer() {
@@ -44,35 +58,42 @@ function Insert() {
         let rerenderObj = JSON.parse(rerender);
 
         await axios.post("http://localhost:8000/building/insert", { username: localStorage.getItem("username") ?? "", geoJson: jsonStr, rerender: rerenderObj }).then((res) => {
-            console.log(res);
+            // console.log(res);
             setCheckingData(res.data);
+            // setResponsePayload(JSON.stringify(res.data));
+            openNotificationWithIcon("success");
         });
     }
 
     async function confirmData(isError: boolean) {
         await axios.post("http://localhost:8000/building/insert/confirm", { id: checkingData.log?._id, confirm: isError }).then((res) => {
-            console.log(res);
+            // console.log(res);
+            openNotificationWithIcon("success");
         });
     }
-
 
     return (
         <div className={styles.container}>
             <div className={styles.headerCheckingContainer}>
                 <div className={styles.checkingTagContainer}>
-                    <p>Data của bạn</p>
-                    {checkingData.json.face.map((f, i) => {
-                        console.log(f);
-                        return <div key={i}>{f}</div>;
-                    })}
-                    <Button type="primary" disabled={checkingData.json.id == ""} onClick={() => confirmData(false)} style={{ marginRight: 20 }}>Correct</Button>
-                    <Button type="primary" disabled={checkingData.json.id == ""} onClick={() => confirmData(true)}>Incorrect</Button>
+                    <div style={{ width: "100%" }}>
+                        <h2>Data của bạn</h2>
+                        {/* <TextArea style={{ width: "90%" }} rows={10} placeholder="Schema" readOnly value={Object.assign({}, JSON.parse(responsePayload))} /> */}
+                        {checkingData.json.nodes.map((f, i) => <p key={i}>{f.map(unit => `${unit} - `)}</p>)}
+                        <p>block_name: {checkingData.json.block_name}</p>
+                        <p>block_height: {checkingData.json.block_height}</p>
+                        <p>color: {checkingData.json.color}</p>
+                    </div>
+                    <div className={styles.groupButtonVertical}>
+                        <Button type="primary" disabled={checkingData.json.id == ""} onClick={() => confirmData(false)} style={{ marginRight: 20 }}>Correct</Button>
+                        <Button type="primary" disabled={checkingData.json.id == ""} danger onClick={() => confirmData(true)}>Incorrect</Button>
+                    </div>
                 </div>
-                <Button type="primary" onClick={showModal}>Primary</Button>
+                <Button type="primary" onClick={showModal}>Submit to DB</Button>
             </div>
             <div className={styles.headerCheckingContainer}>
-                <TextArea style={{ width: "45%" }} rows={40} placeholder="Nhap JSON" onChange={(e) => setJSON(e.target.value)} />
-                <TextArea style={{ width: "45%" }} rows={40} placeholder="Nhap JSON rerender" onChange={(e) => setRerender(e.target.value)} />
+                <TextArea style={{ width: "48%" }} rows={40} placeholder="Nhap GeoJSON" onChange={(e) => setJSON(e.target.value)} />
+                <TextArea style={{ width: "48%" }} rows={40} placeholder="Nhap JSON rerender" onChange={(e) => setRerender(e.target.value)} />
             </div>
             <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
                 <p>Chắc chắn muốn nhập vào database?</p>
