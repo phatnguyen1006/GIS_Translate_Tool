e8bbe9ee1cf3d08cb383b66f67952630cc0e8bbd
P <phatnguyen876@gmail.com>
2022-11-17 09:12:37 +0700

    add env

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,6 @@
-export const DELTA_X1: number = -0.00000631194214;
-export const DELTA_Y1: number = -0.000002728945365;
-export const DELTA_X2: number = -0.00000280644172091706;
-export const DELTA_Y2: number = 0.000006225507939;
+export const DELTA_X1: number = -0.00000631194214;          // Ox
+export const DELTA_Y1: number = -0.000002728945365;         // Ox
+export const DELTA_X2: number = -0.00000280644172091706;    // Oy
+export const DELTA_Y2: number = 0.000006225507939;          // Oy
 export const Z_OFFSET_PER_METER: number = 0.8;
+export const SERVER_URL: string = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:8000";
