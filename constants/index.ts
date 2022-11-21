907c58f3cb89a77f06db799a1a57b983c1fe440c
P <69576826+phatnguyen1006@users.noreply.github.com>
2022-11-21 22:50:28 +0700

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -3,4 +3,4 @@ export const DELTA_Y1: number = -0.000002728945365;         // Ox
 export const DELTA_X2: number = -0.00000280644172091706;    // Oy
 export const DELTA_Y2: number = 0.000006225507939;          // Oy
 export const Z_OFFSET_PER_METER: number = 0.8;
-export const SERVER_URL: string = process.env.NEXT_PUBLIC_SERVER_URL || "https://dragon-wharf-gateway.herokuapp.com";
+export const SERVER_URL: string = process.env.NEXT_PUBLIC_SERVER_URL || "https://dragon-wharf.herokuapp.com";
